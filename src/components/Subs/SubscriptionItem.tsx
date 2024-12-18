3571ce6b65dc2dfffcc6fad44b74a12a58393262
Ali <riffyrr45@gmail.com>
2024-12-18 11:17:39 +0000

    push

--- src/components/Subs/SubscriptionItem.tsx
+++ src/components/Subs/SubscriptionItem.tsx
@@ -5,7 +5,7 @@ import ItemControls from './ItemControls'
 import SubscriptionItemModal from './SubscriptionItemModal'
 
 function SubscriptionItem({ title, amount, type, category }: { title: string, amount: string, type: string, category: string }) {
-  let [isOpen, setIsOpen] = useState(true)
+  let [isOpen, setIsOpen] = useState(false)
 
   function open() {
     setIsOpen(true)
@@ -18,9 +18,11 @@ function SubscriptionItem({ title, amount, type, category }: { title: string, am
   return (
     <div className="w-full h-full  flex items-center border-b  px-2 group cursor-pointer pb-4 pt-4 justify-between"
       key={title}
-      onClick={()=> open()}
+
+    >
+      <div className="flex flex-col w-full items-baseline gap-0.5"
+        onClick={() => open()}
       >
-      <div className="flex flex-col  items-baseline gap-0.5">
 
         <h2 className="text-lg font-medium font-serif tracking-tight text-black group-hover:underline transition-all ease-linear duration-300">
           {title}
@@ -33,16 +35,16 @@ function SubscriptionItem({ title, amount, type, category }: { title: string, am
         </h1>
       </div>
 
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4  justify-end">
         <div className="inline-flex items-center text-xs
         font-normal gap-2 focus:outline-none text-green-600 border p-2 rounded-lg">
-          <CalendarClock className='text-green-600 ' size={16} /> 22 Days
+          <CalendarClock className='text-green-600 ' size={16} />
         </div>
         <ItemControls />
       </div>
 
 
-<SubscriptionItemModal isOpen={isOpen} close={close}/>
+      <SubscriptionItemModal isOpen={isOpen} close={close}  key={title}/>
     </div>
   )
 }
--- src/components/Subs/SubscriptionItemModal.tsx
+++ src/components/Subs/SubscriptionItemModal.tsx
@@ -1,31 +1,102 @@
 'use client'
-import React from 'react'
-import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
+import { useRef } from 'react'
 
-function SubscriptionItemModal({isOpen, close}: {isOpen: boolean, close:any}) {
+function SubscriptionItemModal({ isOpen, close }: { isOpen: boolean, close: any }) {
+    const startDatePicker = useRef<any>(null);
+    const endDatePicker = useRef<any>(null);
+
+    const handleShowStartPicker = () => {
+        if (startDatePicker.current) {
+            startDatePicker.current?.showPicker(); // Trigger the date picker
+        }
+    };
+
+    const handleShowEndPicker = () => {
+        if (endDatePicker.current) {
+            endDatePicker.current?.showPicker(); // Trigger the date picker
+        }
+    };
+
     return (
         <Dialog open={isOpen} as="div" className="relative z-10 focus:outline-none" onClose={close}>
             <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                 <div className="flex min-h-full items-center justify-center p-4">
                     <DialogPanel
                         transition
-                        className="w-full max-w-md rounded-xl bg-white/5 p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+                        className="max-h-[85vh] w-[90vw] max-w-[450px] border rounded-md bg-white data-[state=open]:animate-contentShow overflow-hidden shadow-sm"
                     >
-                        <DialogTitle as="h3" className="text-base/7 font-medium text-white">
-                            Payment successful
+                        <DialogTitle as="div" className="text-base/7 font-medium flex text-black w-full h-full items-center justify-between">
+                            {/* <h3>Dynamics 365</h3>
+
+                            <h3 className='text-gray-400'>£3650 / Yearly</h3> */}
                         </DialogTitle>
-                        <p className="mt-2 text-sm/6 text-white/50">
-                            Your payment has been successfully submitted. We’ve sent you an email with all of the details of your
-                            order.
-                        </p>
-                        <div className="mt-4">
-                            <Button
-                                className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
-                                onClick={close}
-                            >
-                                Got it, thanks!
-                            </Button>
+
+                        <div className='flex flex-col w-full h-full justify-between px-0 py-2'>
+                            <div className='flex flex-col gap-0.5'>
+                                <input className='outline-none border-b border-dashed placeholder:text-gray-400  w-full px-4 py-2 text-base placeholder:text-sm'
+                                    placeholder={'Title'}
+                                    type='text'
+                                    required
+                                    name='task'
+                                    autoFocus
+                                />
+                                <input className=' outline-none border-b border-dashed placeholder:text-gray-400 placeholder:text-sm w-full px-5 py-2 text-sm'
+                                    placeholder={'Description'}
+                                    type='text'
+                                    name='description'
+                                />
+                                <input className=' outline-none border-b border-dashed placeholder:text-gray-400 placeholder:text-sm w-full px-5 py-2 text-sm'
+                                    placeholder={'Amount'}
+                                    type='number'
+                                    name='Amount'
+                                />
+                                <div className='flex w-full items-center justify-between border-b border-dashed'
+
+                                    onClick={handleShowStartPicker}
+                                >
+                                    <input className=' outline-none  placeholder:text-gray-400 placeholder:text-sm w-full px-5 py-2 text-sm'
+                                        placeholder={'Start Date'}
+                                        type='date'
+                                        name='Start Date'
+                                        ref={startDatePicker}
+                                    />
+                                    <p className='text-sm w-full flex items-end justify-end px-5 text-gray-400 font-sans'>Start Date</p>
+                                </div>
+
+                                <div className='flex w-full items-center justify-between border-b border-dashed'
+
+                                    onClick={handleShowEndPicker}
+                                >
+                                    <input className=' outline-none  placeholder:text-gray-400 placeholder:text-sm w-full px-5 py-2 text-sm'
+                                        placeholder={'Start Date'}
+                                        type='date'
+                                        name='Start Date'
+                                        ref={endDatePicker}
+                                    />
+                                    <p className='text-sm w-full flex items-end justify-end px-5 text-gray-400 font-sans'>End Date</p>
+                                </div>
+                            </div>
+
+
+                            <div className='flex items-center justify-between w-full mt-6 px-4 py-2'>
+                                <div className='flex items-center gap-2'>
+                                </div>
+
+                                <div className='flex items-center gap-2'>
+                                    <button className='border text-gray-600 font-medium tracking-tight text-sm px-2.5 py-1 rounded-lg shadow-sm'
+                                    >
+                                        Cancel
+                                    </button>
+                                    <button className='bg-black text-white font-medium h-[30px]
+              tracking-tighter text-sm px-2.5 py-1 rounded-lg shadow-sm w-[80px] text-center'
+                                    >
+                                        Save
+                                    </button>
+                                </div>
+                            </div>
                         </div>
+
                     </DialogPanel>
                 </div>
             </div>
