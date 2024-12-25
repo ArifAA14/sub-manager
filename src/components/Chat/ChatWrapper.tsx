def178c1e240b4c530e110cb718a455e82756eb5
ArifAA14 <riffyrr45@gmail.com>
2024-12-25 19:33:58 +0300

    chat page bug fix

--- src/components/Chat/ChatWrapper.tsx
+++ src/components/Chat/ChatWrapper.tsx
@@ -1,9 +1,10 @@
 import { getAll } from '@/app/actions/SubscriptionService';
 import ChatClient from './ChatClient';
+import SubscriptionEmpty from '../Subs/SubscriptionEmpty';
 
 async function ChatWrapper({ userId }: { userId: string }) {
   const subscriptions = await getAll(userId as string);
-  if (!subscriptions) return null;
+  if (!subscriptions) return <SubscriptionEmpty />
 
   return (
     <ChatClient subscriptions={subscriptions} />
